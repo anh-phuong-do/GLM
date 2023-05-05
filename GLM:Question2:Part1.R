0a95986c20229326be4f53ec70d7ec3fea88ce9b
anh-phuong-do <105778187+anh-phuong-do@users.noreply.github.com>
2023-05-05 11:11:06 +0200

    Update GLM:Question2:Part1.R

--- GLM:Question2:Part1.R
+++ GLM:Question2:Part1.R
@@ -4,3 +4,37 @@
 # Date: 28/4/23
 # Question 2: Model Baseline and Adjacent
 #######################################################
+
+# Not cluster model, proportional odd and continuation
+install.packages("tidyverse")
+library(tidyverse )
+#Import data
+getwd()
+setwd("/Users/macbookair/Documents/UHasselt:Master1:Sem2/GLM/Part 2/")
+EG_data <- read.table("EG.dat", sep = ' ',header = T)
+
+#Count the number of alive, malformed and dead for each litter
+alive <- EG_data %>% count(id, response) %>% filter(response == 1) # all alive in each clusters
+malformed <- EG_data %>% count(id, response) %>% filter(response == 2)
+dead <-  EG_data %>% count(id, response) %>% filter(response == 3)
+dose <- EG_data %>% count(id, dose)
+
+#Merge the data, remove columns, remane and substitute the NA values with 0 values
+data1 <- merge(dose, alive[, -c(2)], by = 'id', all.x = T)
+data1 <- merge(data1, malformed[, -c(2)], by = 'id', all.x = T)
+data1<- merge(data1, dead[, -c(2)], by = 'id', all.x = T)
+colnames(data1) <- c('id', 'dose', 'size','alive', 'malformed', 'dead')
+
+data1[is.na(data1)] <- 0
+
+EG <- data1
+
+# Continuation ratio model
+install.packages("VGAM")
+library(VGAM)
+fit1 = vglm(cbind(alive, malformed, dead)~dose, cratio(parallel = TRUE), EG)
+summary(fit1)
+# proportional odd model
+fit2 = vglm(cbind(alive, malformed, dead)~dose, propodds(reverse = F), EG)
+summary(fit2)
+install.packages("VGLM")
